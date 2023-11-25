a4d1e5758c2ca616ed1d25a9a27c76306afc4151
KirillDolgopolov <Kirdolgopolov.it@gmail.com>
2023-11-25 11:13:28 +0100

    testSecurity

--- Game-of-Dice-Project-main/src/main/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t02/Security/config/SecurityConfig.java
+++ Game-of-Dice-Project-main/src/main/java/cat/itacademy/barcelonactiva/dolgopolov/kirill/s05/t02/Security/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authRequest ->
                         authRequest
                                 .requestMatchers("/auth/**").permitAll()
+                              //  .requestMatchers("/players/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager->
